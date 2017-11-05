08215652ee56a7c24e3d24370d2b98e8b02e8c5b
Filipe Guerra <alias.mac@gmail.com>
2017-11-05 14:27:26 -0800

    Add tags list to blog posts

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -5,11 +5,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDisqusComments from 'react-disqus-comments';
+import { Link } from 'react-router-dom';
+import kebabCase from 'lodash.kebabcase';
 
 export default function Template({ data }) {
 
   const { slug } = data.markdownRemark.fields;
-  const { title } = data.markdownRemark.frontmatter;
+  const { tags, title } = data.markdownRemark.frontmatter;
 
   return (
     <article>
@@ -17,6 +19,12 @@ export default function Template({ data }) {
       <div
         dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
       />
+      <h4>Tags:</h4>
+      <ul>
+        {tags.map(tag => (
+          <li key={kebabCase(tag)}><Link to={`tags/${kebabCase(tag)}`}>{tag}</Link></li>
+        ))}
+      </ul>
       <ReactDisqusComments
         shortname="blog-open-war"
         title={title}
@@ -44,6 +52,7 @@ export const postQuery = graphql`
         slug
       }
       frontmatter {
+        tags
         title
       }
     }
