2c47be8679dca8b2be57284df244c38ab084d4b6
Filipe Guerra <alias.mac@gmail.com>
2017-11-10 23:27:31 -0800

    Add RSS functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,9 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Open War | Make Code, Not War',
+    title: 'Open War',
+    description: 'Make Code, Not War',
+    siteUrl: 'https://blog.open-war.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -21,6 +23,47 @@ module.exports = {
         plugins: [
           {
             resolve: 'gatsby-remark-external-links',
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000
+                  filter: { frontmatter: { published: { eq: true } } }
+                  sort: { order: DESC, fields: [ frontmatter___date ] }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
           },
         ],
       },
