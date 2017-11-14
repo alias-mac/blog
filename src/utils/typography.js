7321af388a4603f8bdab3d9e354806eb9a46a4be
Filipe Guerra <alias.mac@gmail.com>
2017-11-14 00:47:35 -0800

    Style external links
    This will add a nice icon next to an external link, for better
    readability and UX.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,14 +1,39 @@
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
 import Typography from 'typography';
 import CodePlugin from 'typography-plugin-code';
 import Theme from 'typography-theme-github';
+
+import ExternalLinkIcon from '../components/ExternalLinkIcon';
+
+function getExternalLinkUrl(props) {
+
+  const externalLinkString = ReactDOMServer.renderToStaticMarkup(<ExternalLinkIcon {...props} />);
+  const externalLinkBase64 = new Buffer(externalLinkString).toString('base64');
+
+  return `data:image/svg+xml;base64,${externalLinkBase64}`;
+}
 
 Theme.plugins = [
   new CodePlugin(),
 ];
 
-Theme.overrideThemeStyles = (typography, options) => ({
+Theme.overrideThemeStyles = (typography, options, styles) => ({
+
   '.gatsby-highlight pre': {
     boxShadow: '1px 1px 20px rgba(20, 20, 20, 0.27)',
+  },
+
+  'a[href*="://"]:after': {
+    content: `" " url(${getExternalLinkUrl({
+      color: styles.a.color,
+      height: 10,
+      width: 10,
+    })})`,
+    top: '-5px',
+    position: 'relative',
   },
 });
 
