a189c856b365fa0f186f39e10caa9321bfaa7898
Filipe Guerra <alias.mac@gmail.com>
2018-05-28 22:33:34 -0700

    Enable google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,6 +80,18 @@ module.exports = {
       options: {
         pathToConfigModule: 'src/utils/typography',
       },
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-33717220-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
   ],
 };
